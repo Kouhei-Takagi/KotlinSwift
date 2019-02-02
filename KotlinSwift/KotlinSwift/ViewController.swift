2b79c573ec1e5ff8cb742a9260de520ff1ae693f
Kouhei-Takagi <35396762+Kouhei-Takagi@users.noreply.github.com>
2019-02-02 17:28:22 +0900

    Add files via upload

---
+++ KotlinSwift/KotlinSwift/ViewController.swift
@@ -0,0 +1,184 @@
+//
+//  ViewController.swift
+//  Swift2Kotlin
+//
+//  Created by 高木耕平 on 2019/01/24.
+//  Copyright © 2019 高木耕平. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+
+class ViewController: NSViewController {
+
+    
+    @IBOutlet weak var kotlinCode: NSScrollView!
+    @IBOutlet weak var swiftCode: NSScrollView!
+    
+    var swiftCodeString: String!
+    
+    @IBOutlet var kotlinCode1: NSTextView!
+    @IBOutlet var swiftCode1: NSTextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+    override var representedObject: Any? {
+        didSet {
+      
+        }
+    }
+
+    //Copyied from SwiftKotlin. From here...
+    
+    @IBAction func openSwiftFile(_ sender: AnyObject) {
+        let oPanel: NSOpenPanel = NSOpenPanel()
+        oPanel.canChooseDirectories = false
+        oPanel.canChooseFiles = true
+        oPanel.allowsMultipleSelection = false
+        oPanel.allowedFileTypes = ["kt"]
+        oPanel.prompt = "Open"
+        
+        oPanel.beginSheetModal(for: self.view.window!, completionHandler: { (button: NSApplication.ModalResponse) -> Void in
+            if button == NSApplication.ModalResponse.OK {
+                let filePath = oPanel.urls.first!.path
+                let fileHandle = FileHandle(forReadingAtPath: filePath)
+                if let data = fileHandle?.readDataToEndOfFile() {
+                    self.kotlinCode1.textStorage?.beginEditing()
+                    self.kotlinCode1.textColor = NSColor.black
+                    self.kotlinCode1.string = String(data: data, encoding: .utf8) ?? ""
+                    self.kotlinCode1.textStorage?.endEditing()
+                    
+                    
+                }
+            }
+        })
+    }
+    
+    //To here...  Thanks for Giants.
+    
+    @IBAction func format(_ sender: NSButton) {
+        let kotlinCodeString:String = kotlinCode1.string
+
+        //Simple line replacements are here.
+        swiftCodeString = kotlinCodeString.replacingOccurrences(of: "val ", with: "let ")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "println", with: "print")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "fun ", with: "func ")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: ":", with: "->")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "${", with: "\\(")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "for (i in 0..<count - 1)", with: "for i in 0..count")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "arrayOf<String>()", with: "[String]()")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "mapOf<String, Float>()", with: "String: Float]()")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "..", with: "...")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "until", with: "..<")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "when ", with: "switch ")
+        swiftCodeString = swiftCodeString.replacingOccurrences(of: "arrayOf(", with: "[")
+        
+        
+        /*
+        //Multiple lines replacements are here.
+        var sentenceFun = kotlinCodeString!
+        var arr:[String] = sentenceFun.components(separatedBy: "for")
+        var arr2:[String]
+        var token : String
+        var token2 : String
+        var numberFor : Int = arr.count - 1
+        
+        //"for" replacement for aaa {} -> for (aaa) {}
+        while numberFor > 0 {
+            arr2 = arr[(numberFor)].components(separatedBy: "{")
+            token = String(arr[(numberFor)][(arr[(numberFor)].index(of: " ") ?? arr[(numberFor)].startIndex)..<(arr[(numberFor)].index(of: "{") ?? arr[(numberFor)].index(before: arr[(numberFor)].endIndex))]);
+            token.removeFirst(1)
+            token.removeLast(1)
+            token = "for " + "(" + token + ") "
+            arr2.remove(at: 0)
+            token = token + "{" + arr2.joined(separator: "{")
+            arr[(numberFor)] = token
+            sentenceFun = arr.joined()
+            numberFor = numberFor - 1
+        }
+        
+        //"if" replacement if aaa {} -> if (aaa) {}
+        arr = sentenceFun.components(separatedBy: "if")
+        numberFor = arr.count - 1
+        while numberFor > 0 {
+            arr2 = arr[(numberFor)].components(separatedBy: "{")
+            token = String(arr[(numberFor)][(arr[(numberFor)].index(of: " ") ?? arr[(numberFor)].startIndex)..<(arr[(numberFor)].index(of: "{") ?? arr[(numberFor)].index(before: arr[(numberFor)].endIndex))]);
+            token.removeFirst(1)
+            token.removeLast(1)
+            token = "if " + "(" + token + ") "
+            arr2.remove(at: 0)
+            token = token + "{" + arr2.joined(separator: "{")
+            arr[(numberFor)] = token
+            sentenceFun = arr.joined()
+            numberFor = numberFor - 1
+        }
+        /*
+        //"array" replacement [,] -> arrayOf(,)
+        arr = sentenceFun.components(separatedBy: "arrayOf")
+        numberFor = arr.count - 1
+        while numberFor > 0 {
+            token = String(arr[(numberFor)][(arr[(numberFor)].index(of: " ") ?? arr[(numberFor)].startIndex)..<(arr[(numberFor)].index(of: "]") ?? arr[(numberFor)].index(before: arr[(numberFor)].endIndex))]);
+            token.removeFirst(1)
+            token = "arrayOf" + "(" + token + ")" + "\r"
+            arr[(numberFor)] = token
+            sentenceFun = arr.joined()
+            numberFor = numberFor - 1
+        }
+        */
+        //"switch" replacement switch aaa {} -> when (aaa) {}
+        arr = sentenceFun.components(separatedBy: "when")
+        numberFor = arr.count - 1
+        
+        while numberFor > 0 {
+            token = String(arr[(numberFor)][(arr[(numberFor)].index(of: " ") ?? arr[(numberFor)].startIndex)..<(arr[(numberFor)].index(of: "{") ?? arr[(numberFor)].index(before: arr[(numberFor)].endIndex))]);
+            token.removeFirst(1)
+            token.removeLast(1)
+            token = "when " + "(" + token + ") "
+            arr2 = arr[(numberFor)].components(separatedBy: "}")
+            token2 = String(arr2[0][(arr2[0].index(of: "{") ?? arr2[0].startIndex)...(arr2[0].index(of: "}") ?? arr2[0].index(before: arr2[0].endIndex))]);
+            token2 = token2.replacingOccurrences(of: "case", with: "in")
+            token2 = token2.replacingOccurrences(of: "default", with: "else")
+            arr2.remove(at: 0)
+            arr[(numberFor)] = token + token2 + "}" + arr2.joined(separator: "}")
+            sentenceFun = arr.joined()
+            numberFor = numberFor - 1
+        }
+ 
+        */
+ 
+        //kotlinCodeString = sentenceFun
+        swiftCode1.string = swiftCodeString
+    }
+    
+    @IBAction func saveTxtFile(_ sender: Any) {
+        //Copied from http://www.royalcrab.net/wpx/?p=6171
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let fileObject = swiftCode1.string
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let date = formatter.string(from: Date())
+        let fileName = "\(date).swift"
+        let filePath = documentsPath + "/" + fileName
+        
+        do {
+            try fileObject.write(toFile: filePath, atomically: true,
+                                 encoding: String.Encoding.utf8)
+            
+            let alert = NSAlert()
+            alert.messageText = filePath
+            alert.runModal()
+            
+        } catch {
+            let alert = NSAlert()
+            alert.messageText = "Error"
+            alert.runModal()
+        }
+        
+    }
+    
+}
+
